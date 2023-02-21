8c97e51885937f950c59702c45bdea0f66a143ed
Alberto F Alfredo <albertoalfredo.profissional@gmail.com>
2023-02-20 23:50:52 -0300

    Downgrade concluido e geração de página estática pronta

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  srcDir: '',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'ansuztech',
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial />
+  <div>
+    <div class="banner efeito-verde">
+      <img class="banner-img" style="width: 10rem" src="~/assets/logo-branco.png">
+    </div>
+    <div>
+      <h1>Welcome to the homepage</h1>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
+<style lang="css" scoped>
+  .banner{
+      height: 100%;
+      border: 2px #38383800 solid;
+      border-radius: 10px;
+      padding: 1rem;
+      transition: border 2s;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      margin-bottom: 4rem;
+  }
+  .banner:hover{
+      border-color: #fff ;
+  }
+  .banner-img{
+      margin: 2rem;
+      margin-right: 6rem;
+  }
+</style>
+<style>
+@import url("~/assets/styles/reset.css");
+@import url("~/assets/styles/style.css");
+</style>
